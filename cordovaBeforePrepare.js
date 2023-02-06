af4155c14ceaf8f8c61fe335e375604d69da7bf2
Alexander Uvizhev <uvizhe@gmail.com>
2023-02-06 13:35:48 +0300

    build wasm from cordova hook

--- cordovaBeforePrepare.js
+++ cordovaBeforePrepare.js
@@ -1,9 +1,18 @@
-const fs = require('fs');
+const { copyFileSync } = require('fs');
+const { execSync } = require('child_process');
 
 const css_src = 'index.css';
 const css_dst_dir = 'www/css/';
+const wasm_dst_dir = 'node_modules/psh-gui';
+const wasm_pack_cmd = 'wasm-pack build --release --target web --no-typescript';
 
 module.exports = function (context) {
-    fs.copyFileSync(css_src, css_dst_dir + css_src);
+    copyFileSync(css_src, css_dst_dir + css_src);
     console.log('Copied `' + css_src + '` to `' + css_dst_dir + css_src);
+
+    try {
+        execSync(wasm_pack_cmd + ' --out-dir ' + wasm_dst_dir);
+    } catch(error) {
+        console.log('Error building WASM module: ' + error);
+    }
 }

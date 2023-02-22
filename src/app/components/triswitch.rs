0acf28ce01fd04372da5bdd8ab1048e4af1c1036
Alexander Uvizhev <uvizhe@gmail.com>
2023-02-22 17:52:06 +0300

    fix triswitch: don't select disabled variants

--- src/app/components/triswitch.rs
+++ src/app/components/triswitch.rs
@@ -37,8 +37,10 @@ pub fn triswitch(props: &TriswitchProps) -> Html {
             let wrapper = e.target_dyn_into::<web_sys::HtmlElement>().unwrap();
             let radio = wrapper.first_child().unwrap()
                 .dyn_into::<web_sys::HtmlInputElement>().unwrap();
-            let value = radio.value();
-            on_switch.emit(value);
+            if !radio.disabled() {
+                let value = radio.value();
+                on_switch.emit(value);
+            }
         })
     };
 

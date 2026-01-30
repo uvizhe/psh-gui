03ca8c29a4c983f372a0e9181f6e896b1c207116
Alexander Uvizhev <uvizhe@gmail.com>
2026-01-30 18:13:44 +0100

    Hide app content in recents view with FLAG_SECURE

--- src-cordova/www/js/index.js
+++ src-cordova/www/js/index.js
@@ -1,11 +1,6 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    cordova.plugins.backgroundMode.enable();
-    cordova.plugins.backgroundMode.setDefaults({ silent: true });
-
     (async () => {
         const { default: init, main } = await import("./psh_gui.js");
         init().then(() => {
